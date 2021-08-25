bcf3d672e8451ce2a427ce2f91a252fe7a6662d8
Brendan Schulz <brendan.schulz@gmail.com>
2021-08-25 10:03:17 -0500

    a little clean up

--- core.lua
+++ core.lua
@@ -1,7 +1,10 @@
-local function IsComplete(msg, editbox)
-  print(C_QuestLog.IsQuestFlaggedCompleted(msg))
+local function IsComplete(msg)
+  if (msg ~= nil) then
+    print(C_QuestLog.IsQuestFlaggedCompleted(msg))
+  else
+    print("Usage: /ic 12345")
+  end
 end
 
 SLASH_ISCOMPLETE1, SLASH_ISCOMPLETE2 = '/iscomplete', '/ic'
-
 SlashCmdList["ISCOMPLETE"] = IsComplete
